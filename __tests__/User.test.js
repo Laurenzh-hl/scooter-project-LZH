b1bf9c39f8bfb47aa2123f32da05a857c18d9736
Lauren Zell-Huxley <lauren.zell-huxley@hl.co.uk>
2024-10-25 12:53:06 +0100

    added code for User tests

--- __tests__/User.test.js
+++ __tests__/User.test.js
@@ -4,21 +4,33 @@ const User = require("../classes/User.js");
 describe("user.login(password)", () => {
   it.skip("logs a user in if the password is correct", () => {
     // Arrange
+    const u1 = new User("lauren24", "password123", 24);
     // Act
+    u1.login("password123");
     // Assert
+    expect(u1.loggedIn).toBe(true);
   });
 
   it.skip("throws an error if the password is incorrect", () => {
     // Arrange
+    const u2 = new User("josh98", "password321", 25);
     // Act
+    function tryWrongPass () {
+      u2.login("password123");
+    }
     // Assert
+    expect(tryWrongPass).toThrow("incorrect password");
   });
 });
 
 describe("user.logout()", () => {
   it.skip("logs a user out", () => {
     // Arrange
+    const u3 = new User("kate98", "password321", 26);
     // Act
+    u3.login("password321");
+    u3.logout();
     // Assert
+    expect(u3.loggedIn).toBe(false);
   });
 });
