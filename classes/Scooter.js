c2862b1ca621750dc8457a2393a9bcf6dcaf10bb
Lauren Zell-Huxley <lauren.zell-huxley@hl.co.uk>
2024-10-25 11:36:37 +0100

    made Scooter class

--- classes/Scooter.js
+++ classes/Scooter.js
@@ -9,7 +9,53 @@ class Scooter {
     return temp;
   }
 
-  id = Scooter.advanceSerial();
+  constructor (station) {
+    this.station = station;
+    this.user = null;
+    this.charge = 100;
+    this.isBroken = false;
+    this.serial = Scooter.advanceSerial();
+  }
+
+
+  rent(user) {
+    if (this.charge > 20 && this.isBroken === false) {
+      this.station = null;
+      this.user = user;
+    } else if (this.charge > 20 && this.isBroken === true) {
+      throw new Error("scooter needs repair");
+    } else {
+      throw new Error("scooter needs to charge");
+    }
+  }
+
+  dock(station) {
+    this.station = station;
+    this.user = null;
+  }
+
+  charge() {
+    this.charge = 100;
+  }
+
+  repair() {
+    this.isBroken = false;
+  }
 }
+
+// let s1 = new Scooter("Paddington");
+
+// console.log(s1);
+
+// let s2 = new Scooter("Victoria");
+
+// console.log(s2);
+
+
+
+
+
+
+
 
 module.exports = Scooter;
