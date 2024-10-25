d368f753d0347998b83be0a070a8331071ca2916
Lauren Zell-Huxley <lauren.zell-huxley@hl.co.uk>
2024-10-25 11:05:05 +0100

    added class diagram

--- classes/Scooter.js
+++ classes/Scooter.js
@@ -1,5 +1,15 @@
 class Scooter {
   // Scooter code here
+
+  static nextSerial = 1;
+
+  static advanceSerial() {
+    const temp = Scooter.nextSerial;
+    Scooter.nextSerial++;
+    return temp;
+  }
+
+  id = Scooter.advanceSerial();
 }
 
 module.exports = Scooter;
